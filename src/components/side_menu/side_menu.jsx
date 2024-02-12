b5229e706d87df484ce3bfa30fb430e15049ba17
UNilupul <udaranilupul086@gmail.com>
2024-02-12 17:33:34 +0530

    create collapse and expand status

--- src/components/side_menu/side_menu.jsx
+++ src/components/side_menu/side_menu.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./side_menu.css";
 
 import Logo from "../../assets/erp_log.png";
 import ProfilePicture from "../../assets/profile_picture.png";
 
-import { BsArrowLeftSquareFill } from "react-icons/bs";
+import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from "react-icons/bs";
 
 function SideMenu() {
+  const [collapse, setCollapse] = useState(false);
+
   return (
-    <div className="side_menu">
+    <div className={`side_menu ${collapse ? "collapse" : undefined}`}>
       <div className="side_menu-system_logo">
         <img src={Logo} alt="System Logo" />
       </div>
@@ -21,8 +23,13 @@ function SideMenu() {
           <p className="side_menu-user_details-details-name">User Name</p>
           <p className="side_menu-user_details-details-designation">Admin</p>
         </div>
-        <div className="side_menu-user_details-arrow">
-          <BsArrowLeftSquareFill />
+        <div
+          className="side_menu-user_details-arrow"
+          onClick={() => {
+            setCollapse(!collapse);
+          }}
+        >
+          {collapse ? <BsArrowRightSquareFill /> : <BsArrowLeftSquareFill />}
         </div>
       </div>
     </div>
